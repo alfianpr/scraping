1ba3239cee7d2b251a78104684899d99958b674c
alfian <alfianpratama@hotmail.com>
2021-12-28 15:23:36 +0700

    init

--- buka_skincare.py
+++ buka_skincare.py
@@ -4,12 +4,12 @@ from prefect import task, Flow
 from prefect.run_configs import LocalRun
 from prefect.tasks.core.function import FunctionTask
 from datetime import datetime
-from utils import to_csv
+from utils import to_excel
 
 get_access_token = FunctionTask(get_access_token)
 get_products = FunctionTask(get_products)
 clean_df = FunctionTask(clean_df)
-to_csv = FunctionTask(to_csv)
+to_excel = FunctionTask(to_excel)
 
 final_page = 100
 category = "2269"
@@ -20,9 +20,9 @@ def prefect_flow():
         access_token = get_access_token()
         df_scraper = get_products(category, final_page, access_token)
         df_scraper_final = clean_df(df_scraper, timestr)
-        df_csv = to_csv(df_scraper_final, name_file="buka_skincare")
+        df_csv = to_excel(df_scraper_final, name_file="buka_skincare")
     return flow
 
 flow = prefect_flow()
-#flow.run()
-flow.register(project_name="scraping")
+flow.run()
+#flow.register(project_name="scraping")
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import datetime
 
+def to_excel(df, name_file):
+    data = df.to_excel(f'data_scrape/{name_file}_{int(datetime.now().timestamp())}.xlsx')
+    return data
+
 def to_csv(df, name_file):
     data = df.to_csv(f'data_scrape/{name_file}_{int(datetime.now().timestamp())}.csv')
     return data
